0ddf852eeeb8c6de0f77941f3d007580715a9954
fede1817 <Frayg52@gmail.com>
2024-02-20 15:46:52 -0300

    nuevo

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ function Card({id, name, email}){
 
     return(
         <div className="tc grow bg-light-blue br3 pa3 ma2 dib bw2 shadow-5">
-            <img src={"https://robohash.org/"+id+"?size=400x400"} alt={name} />
+            <img src={"https://robohash.org/"+id+"?size=300x300"} alt={name} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
